37bd0eec5f2f58fcece9b5ad255ec2d0afad9cdc
Chris <monkins1010@gmail.com>
2022-03-01 15:19:00 +0000

    Added ETH and ExportManager

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,16 +8,18 @@ var VerusBridge = artifacts.require("./VerusBridge/VerusBridge.sol");
 var Verusaddress = artifacts.require("./VerusBridge/VerusAddressCalculator.sol");
 var VerusInfo = artifacts.require("./VerusBridge/VerusInfo.sol");
 var Token = artifacts.require("./VerusBridge/Token.sol");
+var ExportManager = artifacts.require("./VerusBridge/ExportManager.sol");
 
 // QUESTION: remove all hard coded values like those below (tokenmanvrsctest, etc.) and put them in config or parameters
 // What is the most correct approach / actual best practice?
 const verusNotariserIDS = ["0xb26820ee0c9b1276aac834cf457026a575dfce84", "0x51f9f5f053ce16cb7ca070f5c68a1cb0616ba624", "0x65374d6a8b853a5f61070ad7d774ee54621f9638"];
 const verusNotariserSigner = ["0xD010dEBcBf4183188B00cafd8902e34a2C1E9f41", "0xD010dEBcBf4183188B00cafd8902e34a2C1E9f41", "0xD010dEBcBf4183188B00cafd8902e34a2C1E9f41"];
 const tokenmanvrsctest = ["0xA6ef9ea235635E328124Ff3429dB9F9E91b64e2d","0x0000000000000000000000000000000000000000", false, "vrsctest", "VRSC"];
-const tokenmanbeth = ["0x67460C2f56774eD27EeB8685f29f6CEC0B090B00", "0x0000000000000000000000000000000000000000", false, "bridge.vETH", "BETH"];
+const tokenmanbeth = ["0xffEce948b8A38bBcC813411D2597f7f8485a0689", "0x0000000000000000000000000000000000000000", false, "bridge.vETH", "BETH"];
 const tokenmanUSDC = ["0xf0a1263056c30e221f0f851c36b767fff2544f7f", "0xeb8f08a975ab53e34d8a0330e0d34de942c95926", true, "",""];
+const RinkebyETH = ["0x67460C2f56774eD27EeB8685f29f6CEC0B090B00", "0x0000000000000000000000000000000000000000", false, "Rinkeby ETH", "ETH"];
 
-const launchCurrencies = [tokenmanvrsctest, tokenmanbeth, tokenmanUSDC];
+const launchCurrencies = [tokenmanvrsctest, tokenmanbeth, tokenmanUSDC, RinkebyETH];
 
 const USDCERC20 = "0xeb8f08a975ab53e34d8a0330e0d34de942c95926";
 
@@ -49,6 +51,9 @@ module.exports = async function (deployer) {
     await deployer.deploy(VerusBridge, ProofInst.address, tokenInst.address, serializerInst.address,
         notarizerInst.address, CCEInst.address, "5000000000000000000000");
     const VerusBridgeInst = await VerusBridge.deployed();
+
+    await deployer.deploy(ExportManager, tokenInst.address, VerusBridgeInst.address);
+    await ExportManager.deployed();
 
     await deployer.deploy(VerusInfo, notarizerInst.address, "2000753", "0.7.3-9-rc1", "VETH", true);
     const INFOInst = await VerusInfo.deployed();
