a484ef5b7d2feffe20df258831c4e17eb16d6a69
Chris <monkins1010@gmail.com>
2022-11-02 16:59:08 +0000

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -19,10 +19,6 @@
  */
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
 const privateKeys = ["4d"];
 
 module.exports = {
@@ -62,7 +58,7 @@ module.exports = {
         // NB: It's important to wrap the provider as a function.
         goerli: {
             provider: () => { return new HDWalletProvider(privateKeys, 'wss://goerli.infura.io/ws/v3/') },
-            network_id: 5, // 4= rinkeby 5 = goerli
+            network_id: 5, // 5 = goerli
             confirmations: 0, // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
