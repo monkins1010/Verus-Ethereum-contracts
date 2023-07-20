6533a14f5af72f5ff798d2c860b6cbffc828d83b
Chris <monkins1010@gmail.com>
2023-07-20 19:18:32 +0100

    update tests

--- test/deployed.js
+++ test/deployed.js
@@ -75,4 +75,38 @@ contract("Verus Contracts deployed tests", async(accounts)  => {
         assert.equal(reply.blockNumber, reserveimport[0].endHeight, "Endheight should equal insertion height");
       });
 
+      it("Send 2 ETH in ReserveTransfer to Contract", async () => {
+        const DelegatorInst = await VerusDelegator.deployed();
+        const contractAddress = DelegatorInst.address;
+
+        const contractInstance = new web3.eth.Contract(verusDelegatorAbi.abi, contractAddress);
+  
+    
+        // Send 1 ETH to the contract
+        const sendAmount = web3.utils.toWei("2.003", "ether");
+
+        const CReserveTransfer = {
+            version: 1,
+            currencyvalue: { currency: "0x67460C2f56774eD27EeB8685f29f6CEC0B090B00", amount: 200000000 }, // currency sending from ethereum
+            flags: 1,
+            feecurrencyid: "0xA6ef9ea235635E328124Ff3429dB9F9E91b64e2d", // fee is vrsctest pre bridge launch, veth or others post.
+            fees: 2000000,
+            destination: { destinationtype: 2, destinationaddress: "0x9bB2772Aa50ec96ce1305D926B9CC29b7c402bAD" }, // destination address currecny is going to
+            destcurrencyid: "0xA6ef9ea235635E328124Ff3429dB9F9E91b64e2d",   // destination currency is vrsc on direct. bridge.veth on bounceback
+            destsystemid: "0x0000000000000000000000000000000000000000",     // destination system not used 
+            secondreserveid: "0x0000000000000000000000000000000000000000"    // used as return currency type on bounce back
+          }
+        let reply
+        try {
+            reply = await contractInstance.methods.sendTransfer(CReserveTransfer).send({ from: accounts[0], gas: 6000000, value: sendAmount }); 
+        } catch(e) {
+            console.log(e)
+            assert.isTrue(false);
+        }
+        // Get the contract balance after sending ETH exportHeights
+        const previousStartHeight = await DelegatorInst.exportHeights.call(0);
+        let reserveimport = await DelegatorInst.getReadyExportsByRange.call(0, reply.blockNumber + 10);
+        assert.isTrue(true);
+      });
+
 });
