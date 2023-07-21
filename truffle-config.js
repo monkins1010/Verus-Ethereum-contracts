f353113dd2b225ef3b1c87e0275fd4f7d9beb839
Chris <monkins1010@gmail.com>
2023-07-21 17:34:37 +0100

    Add windows test message

--- truffle-config.js
+++ truffle-config.js
@@ -63,7 +63,7 @@ module.exports = {
             network_id: 5, // 5 = goerli
             confirmations: 0, // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
-            skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+            skipDryRun: false, // Skip dry run before migrations? (default: false for public nets )
             networkCheckTimeout: 10000,
         },
 
