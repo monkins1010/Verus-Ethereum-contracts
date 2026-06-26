d841f0371ac4e89bc85eb07dbda56d2594378e5b
monkins <monkins1010@gmail.com>
2026-06-26 10:16:41 +0100

    correct vusdc address and checksum

--- contracts/VerusNotarizer/NotarizationSerializer.sol
+++ contracts/VerusNotarizer/NotarizationSerializer.sol
@@ -17,7 +17,7 @@ contract NotarizationSerializer is VerusStorage {
     address immutable DAI;
     address immutable MKR;
     address immutable tBTC = 0xf87F6d4412dAd7c4452e8293850Df5327f02C308; // tBTC iaddress iS8TfRPfVpKo5FVfSUzfHBQxo9KuzpnqLU as hex
-    address immutable vUSDC = 0x67d6df2ccc766daffb1f36ccc9b8b5f0db5cd11b; // vUSDC iaddress i61cV2uicKSi1rSMQCBNQeSYC3UAi9GVzd as hex
+    address immutable vUSDC = 0x1Bd15cDbf0B5B8c9CC361FFBaf6D76cc2CdfD667; // vUSDC iaddress i61cV2uicKSi1rSMQCBNQeSYC3UAi9GVzd as hex
 
     constructor(address vETH, address Bridge, address Verus, address Dai, address Mkr){
 
